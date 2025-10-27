db1036afd403c404ca26e54f69435b796d62061e
Laura <dautila85913@th-nuernberg.de>
2025-10-27 13:12:51 +0100

    Homework B01 - Method Types and Properties

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -1,73 +1,69 @@
+import { Coordinate } from "../coordinates/Coordinate";
+
 export const DEFAULT_DELIMITER: string = '.';
 export const ESCAPE_CHARACTER = '\\';
 
-/**
- * A name is a sequence of string components separated by a delimiter character.
- * Special characters within the string may need masking, if they are to appear verbatim.
- * There are only two special characters, the delimiter character and the escape character.
- * The escape character can't be set, the delimiter character can.
- * 
- * Homogenous name examples
- * 
- * "oss.cs.fau.de" is a name with four name components and the delimiter character '.'.
- * "///" is a name with four empty components and the delimiter character '/'.
- * "Oh\.\.\." is a name with one component, if the delimiter character is '.'.
- */
 export class Name {
 
     private delimiter: string = DEFAULT_DELIMITER;
     private components: string[] = [];
 
-    /** Expects that all Name components are properly masked */
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.components = other;
+        this.delimiter = delimiter ?? DEFAULT_DELIMITER;
     }
 
-    /**
-     * Returns a human-readable representation of the Name instance using user-set special characters
-     * Special characters are not escaped (creating a human-readable string)
-     * Users can vary the delimiter character to be used
-     */
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(delimiter);
     }
 
-    /** 
-     * Returns a machine-readable representation of Name instance using default special characters
-     * Machine-readable means that from a data string, a Name can be parsed back in
-     * The special characters in the data string are the default characters
-     */
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        const escapedComponents = this.components.map(component => {
+            let escaped = '';
+            for (let i = 0; i < component.length; i++) {
+                const char = component[i];
+                if (char === DEFAULT_DELIMITER || char === ESCAPE_CHARACTER) {
+                    escaped += ESCAPE_CHARACTER;
+                }
+                escaped += char;
+            }
+            return escaped;
+        });
+        
+        return escapedComponents.join(DEFAULT_DELIMITER)
     }
 
-    /** Returns properly masked component string */
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        return this.components[i];
     }
 
-    /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components[i] = c;
     }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+    // @methodtype get-method
+    public getNoComponents(): number {
+        return this.components.length;
     }
 
-    /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 0, c);
     }
 
-    /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 1);
     }
 
 }
